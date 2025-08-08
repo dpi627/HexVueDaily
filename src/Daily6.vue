886853fc7bf66d671bfa159e6ce94e5ede31a91c
brian <dpi.studio@gmail.com>
2025-08-08 14:51:23 +0800

    fix: 修正 Daily6.vue 中的 v-for 語法，移除不必要的 key 參數

--- src/Daily6.vue
+++ src/Daily6.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div 
         class="col-md-6 mb-3" 
-        v-for="(fruit, key) in fruitData" 
+        v-for="fruit in fruitData" 
         :key="fruit.title"
       >
         <div
