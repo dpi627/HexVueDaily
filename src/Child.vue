94cac4106e8470bd93f4ac0c89cb70300bed4aab
Brian <dpi.studio@gmail.com>
2025-08-13 19:24:17 +0800

    feat: 新增 Child.vue 和 Parent.vue，實作計數器功能

---
+++ src/Child.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  initialCount: {
+    type: Number,
+    default: 0
+  }
+});
+
+const emit = defineEmits(['update-count']);
+
+const localCount = ref(props.initialCount);
+
+const increment = () => {
+  localCount.value++;
+  emit('update-count', localCount.value);
+};
+</script>
---
+++ src/Parent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <Counter :initialCount="count" @update-count="handleUpdate" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Counter from './Child.vue';
+
+const count = ref(0);
+
+const handleUpdate = (newValue) => {
+  count.value = newValue;
+};
+</script>
