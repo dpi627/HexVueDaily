394af9b2df34120230a2d669075678a0bc77fa43
brian <dpi.studio@gmail.com>
2025-08-14 13:05:59 +0800

    feat: 更新 main.js 引入 Daily10.vue，新增計數器功能

---
+++ src/Daily10.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    <h1>原始數量: {{ count }}</h1>
+    <h1>三倍數量: {{ tripleCount }} (computed)</h1> 
+    <h1>三倍數量: {{ calculateTripleCount() }} (method)</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const count = ref(2);
+
+const tripleCount = computed(() => {
+  return count.value * 3;
+});
+
+function calculateTripleCount() {
+  return count.value * 3;
+}
+</script>
