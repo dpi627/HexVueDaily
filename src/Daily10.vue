fc985424d4898f6c447d9e0d23e660c622d817cf
brian <dpi.studio@gmail.com>
2025-08-14 13:31:55 +0800

    feat: 移除三倍數量計算，更新 Daily10.vue 結構，新增水果顯示功能

--- src/Daily10.vue
+++ src/Daily10.vue
@@ -1,22 +1,55 @@
+<!-- 請不要修改下面內容 -->
 <template>
   <div id="app" class="container">
-    <h1>原始數量: {{ count }}</h1>
-    <h1>三倍數量: {{ tripleCount }} (computed)</h1>
-    修改三倍數量: <input v-model="tripleCount" /> (反向計算)
+    <div class="total-price mt-3">
+      <h4>總價：{{ totalPrice }} 元</h4>
+    </div>
+    
+    <div class="row">
+      <template v-for="(fruit, index) in fruitData" :key="index">
+        <div class="col-md-6 mb-3">
+          <div class="card" style="width: 100%;">
+            <img :src="fruit.imgUrl" class="card-img-top w-25" :alt="fruit.title">
+            <div class="card-body">
+              <h5 class="card-title">水果名稱：{{ fruit.title }}</h5>
+              <p class="card-text my-2">價錢：{{ fruit.price }}</p>
+              <p class="card-text mb-3">數量：{{ fruit.count }}</p>
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
+<!-- -->
 
 <script setup>
 import { ref, computed } from "vue";
 
-const count = ref(2);
-
-const tripleCount = computed({
-  get() {
-    return count.value * 3;
+const fruitData = ref([
+  {
+    title: 'apple',
+    price: 25,
+    count: 50,
+    imgUrl: 'https://i.imgur.com/w4sYWlS.jpeg'
   },
-  set(newValue) {
-    count.value = newValue / 3; // 反向計算
+  {
+    title: 'orange',
+    price: 15,
+    count: 20,
+    imgUrl: 'https://i.imgur.com/PSmzmXi.jpeg'
+  },
+  {
+    title: 'strawberry',
+    price: 45,
+    count: 10,
+    imgUrl: 'https://i.imgur.com/FIMmh6h.png',
+  },
+  {
+    title: 'kiwi',
+    price: 55,
+    count: 20,
+    imgUrl: 'https://i.imgur.com/TIA6v4m.jpeg'
   }
-});
+]);
 </script>
