4156475b7c282d952df1c65d5d2f21c304b6f93a
brian <dpi.studio@gmail.com>
2025-08-15 16:50:52 +0800

    feat: 移除電子郵件驗證，新增姓名和電話輸入欄位及錯誤提示

--- src/Daily11.vue
+++ src/Daily11.vue
@@ -1,28 +1,41 @@
 <template>
-  <label for="email">Email：</label>
-  <input id="email" v-model="email" />
-  <span v-if="emailError">{{ emailError }}</span>
+  <div class="mb-2">
+    <label for="username">姓名：</label>
+    <input id="username" v-model="user.username" />
+    <span v-if="errors.username">{{ errors.username }}</span>
+  </div>
+  <div class="mb-2">
+    <label for="phone">電話：</label>
+    <input id="phone" v-model="user.phone" />
+    <span v-if="errors.phone">{{ errors.phone }}</span>
+  </div>
 </template>
 
 <script setup>
 import { ref, watch } from 'vue';
 
-const email = ref('');
-const emailError = ref('');
-
-watch(email, (newVal) => {
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(newVal)) {
-    emailError.value = 'email 格式不正確';
-  } else {
-    emailError.value = '';
-  }
+const user = ref({
+  username: '',
+  phone: ''
 });
-</script>
 
-<style>
-span {
-  color: red;
-  font-size: 0.8em;
-}
-</style>
+const errors = ref({
+  username: '',
+  phone: ''
+});
+
+watch(user, (newVal) => {
+  if (newVal.username.length < 3) {
+    errors.value.username = '需要正確的輸入名稱';
+  } else {
+    errors.value.username = '';
+  }
+
+  const phonePattern = /^[0-9]{10}$/;
+  if (!phonePattern.test(newVal.phone)) {
+    errors.value.phone = '需要正確的輸入電話號碼';
+  } else {
+    errors.value.phone = '';
+  }
+}, { deep: true });
+</script>
