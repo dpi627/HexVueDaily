f6d5a5cf075dfa17aa3122b9bc190e56f54b56be
brian <dpi.studio@gmail.com>
2025-08-16 07:13:00 +0800

    feat: 更新 Daily11.vue，調整姓名輸入欄位及錯誤提示，移除電話欄位

--- src/Daily11.vue
+++ src/Daily11.vue
@@ -1,41 +1,43 @@
+<!-- 請不要修改下面內容 -->
 <template>
-  <div class="mb-2">
-    <label for="username">姓名：</label>
-    <input id="username" v-model="user.username" />
-    <span v-if="errors.username">{{ errors.username }}</span>
-  </div>
-  <div class="mb-2">
-    <label for="phone">電話：</label>
-    <input id="phone" v-model="user.phone" />
-    <span v-if="errors.phone">{{ errors.phone }}</span>
+  <div>
+    <form>
+      <div class="m-5">
+        <label for="username">購買人姓名：</label>
+        <input id="username" v-model="user.username" />
+        <span v-if="errors.username">{{ errors.username }}</span>
+      </div>
+    </form>
   </div>
 </template>
+<!-- -->
 
 <script setup>
 import { ref, watch } from 'vue';
 
 const user = ref({
   username: '',
-  phone: ''
 });
 
 const errors = ref({
   username: '',
-  phone: ''
 });
 
-watch(user, (newVal) => {
-  if (newVal.username.length < 3) {
-    errors.value.username = '需要正確的輸入名稱';
-  } else {
-    errors.value.username = '';
+// 監聽 user.username 的變化
+watch(() => user, (newVal) => {
+  if (newVal.value.username.length != 3) {
+    errors.value.username = "需要輸入正確名稱"
   }
+  else {
+    errors.value.username = ""
+  }
+}, { deep: true, immediate: true });
 
-  const phonePattern = /^[0-9]{10}$/;
-  if (!phonePattern.test(newVal.phone)) {
-    errors.value.phone = '需要正確的輸入電話號碼';
-  } else {
-    errors.value.phone = '';
-  }
-}, { deep: true });
 </script>
+
+<style>
+span {
+  color: red;
+  font-size: 0.8em;
+}
+</style>
