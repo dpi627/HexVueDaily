18c11207736704124c3122b81411aecfd45fe00f
brian <dpi.studio@gmail.com>
2025-08-18 15:41:17 +0800

    feat: 新增 Daily12.vue，包含 Header 和 Footer 組件，更新 main.js 引入

---
+++ src/Daily12.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header />
+  <div>
+    <h2>I'm Page Content</h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur iusto nobis magni sapiente numquam, impedit mollitia ea, accusantium cumque cupiditate explicabo voluptates possimus recusandae quo fugit. Harum tenetur nihil ipsum?
+  </div>
+  <Footer />
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue';
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>I'm Header</h1>
+</template>
