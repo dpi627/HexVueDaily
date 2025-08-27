efa69982434ece0256e88d80f70a78bad04e4661
brian <dpi.studio@gmail.com>
2025-08-27 12:00:29 +0800

    feat: 修改 Daily18.vue 以實現登入功能
    - 將註冊表單更改為登入表單
    - 更新 v-model 以使用 emailSignIn 和 passwordSignIn
    - 移除不必要的註冊相關欄位和邏輯
    - 新增登入按鈕和回應訊息顯示

--- src/Daily18.vue
+++ src/Daily18.vue
@@ -1,24 +1,20 @@
 <template>
   <div id="app">
     <div class="container mt-2">
-      <h2>註冊</h2>
+      <h2>登入</h2>
       <form>
         <div class="form-group">
-          <label for="exampleInputEmail1">Email</label>
-          <input v-model="emailSignUp" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="請輸入信箱">
+          <label for="exampleInputEmail2">Email</label>
+          <input v-model="emailSignIn" type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder="請輸入信箱">
         </div>
         <div class="form-group my-3">
-          <label for="exampleInputPassword1">Password</label>
-          <input v-model="passwordSignUp" type="password" class="form-control" id="exampleInputPassword1" placeholder="請輸入密碼">
+          <label for="exampleInputPassword2">Password</label>
+          <input v-model="passwordSignIn" type="password" class="form-control" id="exampleInputPassword2" placeholder="請輸入密碼">
         </div>
-        <div class="form-group my-3">
-          <label for="exampleInputNickName">Nick Name</label>
-          <input v-model="nickname" type="text" class="form-control" id="exampleInputNickName" placeholder="請輸入暱稱">
-        </div>
-        <button @click="signUp" type="button" class="btn btn-primary">註冊</button>
+        <button @click="signIn" type="button" class="btn btn-success">登入</button>
       </form>
-      <template v-if="messageSignUp">
-        <p :class="{'mt-2': true, 'text-danger': isErrorSignUp, 'text-success': !isErrorSignUp}"> {{ messageSignUp }}</p>       
+      <template v-if="responseMessage">
+        <p class="h3" :class="{'mt-3': true, 'text-danger': isErrorMessage, 'text-success': !isErrorMessage}"> {{ responseMessage }}</p>        
       </template>
     </div>
   </div>
@@ -26,29 +22,17 @@
 
 <script setup>
 import { ref } from "vue";
-import axios from 'axios'
-
 
 const api = 'https://todolist-api.hexschool.io';
 
-const emailSignUp = ref('');
-const passwordSignUp = ref('');
-const nickname = ref('');
-const messageSignUp = ref('');
-const isErrorSignUp = ref(false);
+const emailSignIn = ref('');
+const passwordSignIn = ref('');
+  
+const responseMessage = ref('');
+const isErrorMessage = ref(false);
+const token = ref('');
 
-const signUp = async () => {
-  try {
-    const response = await axios.post(`${api}/users/sign_up`, {
-      email: emailSignUp.value,
-      password: passwordSignUp.value,
-      nickname: nickname.value,
-    });
-    messageSignUp.value = '註冊成功. UID: ' + response.data.uid;
-    isErrorSignUp.value = false;
-  } catch (error) {
-    messageSignUp.value = '註冊失敗: ' + error.response.data.message;
-    isErrorSignUp.value = true;
-  }
+const signIn = async () => {
+
 };
 </script>
