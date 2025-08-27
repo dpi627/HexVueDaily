85792ab09c6793a47342e171a8422c49061b4a04
brian <dpi.studio@gmail.com>
2025-08-27 13:36:49 +0800

    feat: 更新 Daily18.vue 以實現登入功能
    - 調整表單結構以改善可讀性
    - 增加登入功能的錯誤處理
    - 顯示登入成功或失敗的回應訊息

--- src/Daily18.vue
+++ src/Daily18.vue
@@ -5,34 +5,53 @@
       <form>
         <div class="form-group">
           <label for="exampleInputEmail2">Email</label>
-          <input v-model="emailSignIn" type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder="請輸入信箱">
+          <input v-model="emailSignIn" type="email" class="form-control" id="exampleInputEmail2"
+            aria-describedby="emailHelp" placeholder="請輸入信箱">
         </div>
         <div class="form-group my-3">
           <label for="exampleInputPassword2">Password</label>
-          <input v-model="passwordSignIn" type="password" class="form-control" id="exampleInputPassword2" placeholder="請輸入密碼">
+          <input v-model="passwordSignIn" type="password" class="form-control" id="exampleInputPassword2"
+            placeholder="請輸入密碼">
         </div>
         <button @click="signIn" type="button" class="btn btn-success">登入</button>
       </form>
       <template v-if="responseMessage">
-        <p class="h3" :class="{'mt-3': true, 'text-danger': isErrorMessage, 'text-success': !isErrorMessage}"> {{ responseMessage }}</p>        
+        <p class="h3" :class="{ 'mt-3': true, 'text-danger': isErrorMessage, 'text-success': !isErrorMessage }"> {{
+          responseMessage }}</p>
       </template>
     </div>
   </div>
 </template>
 
 <script setup>
+import axios from "axios";
+import { createWebSocketModuleRunnerTransport } from "vite/module-runner";
 import { ref } from "vue";
 
 const api = 'https://todolist-api.hexschool.io';
 
 const emailSignIn = ref('');
 const passwordSignIn = ref('');
-  
+
 const responseMessage = ref('');
 const isErrorMessage = ref(false);
 const token = ref('');
 
 const signIn = async () => {
-
+  try {
+    const res = await axios.post(`${api}/users/sign_in`, {
+      "email": emailSignIn.value,
+      "password": passwordSignIn.value
+    })
+    isErrorMessage.value = false
+    console.log(res)
+    token.value = res.data.token
+    responseMessage.value = `登入成功: ${res.data.nickname} [token: ${token.value}]`
+  }
+  catch (error) {
+    isErrorMessage.value = true
+    console.log(error)
+    responseMessage.value = `登入失敗: ${error.response.data.message}`
+  }
 };
 </script>
